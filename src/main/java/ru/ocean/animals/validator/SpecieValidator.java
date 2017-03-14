9b72372cfb288608fd7be64ff53e56b7ed5dac16
Dubinets Aleksandr <dubinets.av@gmail.com>
2017-03-15 08:59:09 +1000

    specie validation

---
+++ src/main/java/ru/ocean/animals/validator/SpecieValidator.java
@@ -0,0 +1,34 @@
+package ru.ocean.animals.validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import ru.ocean.animals.model.Specie;
+import ru.ocean.animals.service.SpecieService;
+
+@Component
+public class SpecieValidator implements Validator {
+
+    @Autowired
+    private SpecieService specieService;
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Specie.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Specie specie = (Specie) o;
+
+        if(specie.getSpecie_name_lat() == null) {
+            errors.rejectValue("specie_name_lat", "Null.value");
+        }
+
+        if(specie.getSpecie_name_ru() == null) {
+            errors.rejectValue("specie_name_ru", "Null.value");
+        }
+
+    }
+}
